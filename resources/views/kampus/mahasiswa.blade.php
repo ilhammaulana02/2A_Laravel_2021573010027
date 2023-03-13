d1646c0919f1c546ac307842cb726f080f95b673
ilhammaulana02 <ilhammaulana020603@gmail.com>
2023-03-13 12:47:22 +0700

    progres

--- resources/views/kampus/mahasiswa.blade.php
+++ resources/views/kampus/mahasiswa.blade.php
@@ -4,17 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Daftar Mahasiswa</title>
+    <title>Data Mahasiswa</title>
 </head>
 <body>
     <h1>Daftar Mahasiswa</h1>
     <ol>
         <?php
         foreach ($mahasiswa as $nama) {
-        echo "<li>$nama</li>";
+            echo "<li>$nama</li>";
         }
         ?>
     </ol>
-    <div>Copyright &copy; <?php echo date("Y"); ?>Program Studi Teknik Informatika - PNL</div>
+    <div>
+        Copyright &copy; <?php echo date("Y"); ?> Program Studi Teknik Informatika - PNL
+    </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -29,105 +29,105 @@ Route::get('/belajar',function(){
     echo '<p>Sedang belajar Laravel</p>';
 });
 
-Route::get('/mahasiswa/profil/coba',function(){
-    echo '<h2 style="text-align: center"><u>Welcome Profil Coba</u></h2>';
-});
+// Route::get('/mahasiswa/profil/coba',function(){
+//     echo '<h2 style="text-align: center"><u>Welcome Profil Coba</u></h2>';
+// });
 
-// 3. Route Parameter
-Route::get('/mahasiswa/{nama}',function($nama){
-    return "Tampilkan data mahasiswa bernama $nama";
-});
+// // 3. Route Parameter
+// Route::get('/mahasiswa/{nama}',function($nama){
+//     return "Tampilkan data mahasiswa bernama $nama";
+// });
 
-// 4. Route Parameter dengan 2 Parameter
-Route::get('/stok_barang/{jenis}/{merek}',function($jenis,$merek){
-    return "Cek sisa stok untuk $jenis $merek";
-});
+// // 4. Route Parameter dengan 2 Parameter
+// Route::get('/stok_barang/{jenis}/{merek}',function($jenis,$merek){
+//     return "Cek sisa stok untuk $jenis $merek";
+// });
 
-Route::get('/stok_barang/{jenis}/{merek}',function($a,$b){
-    echo "Cek sisa stok untuk $a $b";
-});
+// Route::get('/stok_barang/{jenis}/{merek}',function($a,$b){
+//     echo "Cek sisa stok untuk $a $b";
+// });
 
 
-// 5. Route dengan Optional Parameter
-Route::get('/stok_barang/{jenis?}/{merek?}',function($a='smartphone',$b='samsung'){
-    return "Cek sisa stok untuk $a $b";
-});
+// // 5. Route dengan Optional Parameter
+// Route::get('/stok_barang/{jenis?}/{merek?}',function($a='smartphone',$b='samsung'){
+//     return "Cek sisa stok untuk $a $b";
+// });
 
-// 6. Route Parameter dengan Regular Expression
-Route::get('/user/{id}',function($id){
-    return "Tampilkan user dengan id = $id";
-});
+// // 6. Route Parameter dengan Regular Expression
+// Route::get('/user/{id}',function($id){
+//     return "Tampilkan user dengan id = $id";
+// });
 
-Route::get('/user/{id}',function($id){
-    return "Tampilkan user dengan id =  $id";
-})->where('id','[0-9]+');
+// Route::get('/user/{id}',function($id){
+//     return "Tampilkan user dengan id =  $id";
+// })->where('id','[0-9]+');
 
-Route::get('/user/{id}',function($id){
-    return "Tampilkan user dengan id = $id";
-})->where('id','[A-Z]{2}[0-9]+');
+// Route::get('/user/{id}',function($id){
+//     return "Tampilkan user dengan id = $id";
+// })->where('id','[A-Z]{2}[0-9]+');
 
-// 7. Route Redirect
-Route::get('/hubungi-kami',function(){
-    return "<h1>Hubungi Kami</h1>";
-});
+// // 7. Route Redirect
+// Route::get('/hubungi-kami',function(){
+//     return "<h1>Hubungi Kami</h1>";
+// });
 
-Route::redirect('/contact-us','/hubungi-kami');
+// Route::redirect('/contact-us','/hubungi-kami');
 
-// 8. Route Group
-Route::get('/admin/mahasiswa',function(){
-    return "<h1>Daftar Mahasiswa</h1>";
-});
-Route::get('/admin/dosen',function(){
-    return "<h1>Daftar Dosen</h1>";
-});
-Route::get('/admin/karyawan',function(){
-    return "<h1>Daftar Karyawan</h1>";
-});
+// // 8. Route Group
+// Route::get('/admin/mahasiswa',function(){
+//     return "<h1>Daftar Mahasiswa</h1>";
+// });
+// Route::get('/admin/dosen',function(){
+//     return "<h1>Daftar Dosen</h1>";
+// });
+// Route::get('/admin/karyawan',function(){
+//     return "<h1>Daftar Karyawan</h1>";
+// });
 
-Route::prefix('/admin')->group(function(){
-    Route::get('/mahasiswa',function(){
-        echo "<h1>Daftar Mahasiswa</h1>";
-    });
-    Route::get('/dosen',function(){
-        echo "<h1>Daftar Dosen</h1>";
-    });
-    Route::get('/karyawan',function(){
-        echo "<h1>Daftar Karyawan</h1>";
-    });
-});
+// Route::prefix('/admin')->group(function(){
+//     Route::get('/mahasiswa',function(){
+//         echo "<h1>Daftar Mahasiswa</h1>";
+//     });
+//     Route::get('/dosen',function(){
+//         echo "<h1>Daftar Dosen</h1>";
+//     });
+//     Route::get('/karyawan',function(){
+//         echo "<h1>Daftar Karyawan</h1>";
+//     });
+// });
 
-// 9. Route Fallback
-Route::fallback(function(){
-    return "Maaf, alamat tidak ditemukan";
-});
+// // 9. Route Fallback
+// Route::fallback(function(){
+//     return "Maaf, alamat tidak ditemukan";
+// });
 
 // 10. Route Priority
-Route::get('/buku/1',function(){
-    return "Buku ke-1";
-});
-Route::get('/buku/1',function(){
-    return "Buku saya ke-1";
-});
-Route::get('/buku/1',function(){
-    return "Buku kita ke-1";
-});
+// Route::get('/buku/1',function(){
+//     return "Buku ke-1";
+// });
+// Route::get('/buku/1',function(){
+//     return "Buku saya ke-1";
+// });
+// Route::get('/buku/1',function(){
+//     return "Buku kita ke-1";
+// });
 
-Route::get('/buku/{a}',function($a){
-    return "Buku ke-$a";
-});
-Route::get('/buku/{b}',function($b){
-    return "Buku saya ke-$b";
-});
-Route::get('/buku/{c}',function($c){
-    return "Buku kita ke-$c";
-});
+// Route::get('/buku/{a}',function($a){
+//     return "Buku ke-$a";
+// });
+// Route::get('/buku/{b}',function($b){
+//     return "Buku saya ke-$b";
+// });
+// Route::get('/buku/{c}',function($c){
+//     return "Buku kita ke-$c";
+// });
 
-Route::get('mahasiswa/andi',function(){
-    echo "Halaman mahasiswa andi";
-});
-Route::get('/mahasiswa/andi',function(){
-    echo "Halaman mahasiswa andi";
-});
+// Route::get('mahasiswa/andi',function(){
+//     echo "Halaman mahasiswa andi";
+// });
+// Route::get('/mahasiswa/andi',function(){
+//     echo "Halaman mahasiswa andi";
+// });
 
 /* 11.Melihat Daftar Route
     Bisa buka terminal di VSCode(pilih menu Terminal -> New Terminal), Lalu jalankan perintah php artisan 
@@ -140,42 +140,108 @@ Langkah-langkah untuk membuat view adalah:
 */
 
 // 1. Buat route pada halaman web.php yang akan mengembalikan view
-Route::get('/home',function(){
-    return view('halaman_home');
-});
+// Route::get('/home',function(){
+//     return view('halaman_home');
+// });
 
-Route::get('/mahasiswa',function(){
-    return view('mahasiswa');
-});
+// Route::get('/mahasiswa',function(){
+//     return view('mahasiswa');
+// });
 
-Route::get('/mahasiswa',function(){
-    return view('kampus.mahasiswa');
-});
+//2. Membuat Struktur Folder View
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa');
+// });
 
-Route::get('/mahasiswa',function(){
-    return view('kampus/mahasiswa');
-});
+// Route::get('/mahasiswa',function(){
+//     return view('kampus/mahasiswa');
+// });
 
-Route::get('/mahasiswa',function(){
-    $arrMahasiswa = [
-    "mahasiswa01"=>"Indra Kenz",
-    "mahasiswa02"=>"Doni Salmanan",
-    "mahasiswa01"=>"Ulfi Rizkia",
-    "mahasiswa01"=>"Deliana Putri"
-    ];
-    return view('kampus.mahasiswa',$arrMahasiswa);
-});
+// Mengirim Data ke View Sebagai Argumen
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa',["mahasiswa01"=>"Maulana Sultansyah"]);
+// })
 
-Route::get('/mahasiswa',function(){
-    $arrMahasiswa = ["Doni Sadikin","Syadzwina Sahara","Deliana Putri","Indra Kenz"];
-    return view('kampus.mahasiswa',['mahasiswa'=>$arrMahasiswa]);
-});
+// Route::get('/mahasiswa',function(){
+//     $arrMahasiswa = [
+//     "mahasiswa01"=>"Indra Kenz",
+//     "mahasiswa02"=>"Doni Salmanan",
+//     "mahasiswa03"=>"Ulfi Rizkia",
+//     "mahasiswa04"=>"Deliana Putri"
+//     ];
+//     return view('kampus.mahasiswa',$arrMahasiswa);
+// });
 
-Route::get('/mahasiswa',function(){
-    return view('kampus.mahasiswa')->with('mahasiswa01','Risa Lestari');
-});
+// Route::get('/mahasiswa',function(){
+//     $arrMahasiswa = ["Doni Sadikin","Syadzwina Sahara","Deliana Putri","Indra Kenz"];
+//     return view('kampus.mahasiswa',['mahasiswa'=>$arrMahasiswa]);
+// });
 
-Route::get('/mahasiswa',function(){
-    $arrMahasiswa = ["Doni Sadikin","Syadzwina Sahara","Deliana Putri","Indra Kenz"];
-    return view('kampus.mahasiswa')->with('mahasiswa',$arrMahasiswa);
-});
+//Mengirim Data ke View menggunakan Method With
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa')->with('mahasiswa01','Risa Lestari');
+// });
+
+//Dengan banyak data dan menampilkan dengan looping
+// Route::get('/mahasiswa',function(){
+//     $arrMahasiswa = ["Doni Sadikin","Syadzwina Sahara","Deliana Putri","Indra Kenz"];
+//     return view('kampus.mahasiswa')->with('mahasiswa',$arrMahasiswa);
+// });
+
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa')
+//     ->with('mahasiswa01','Doni Sadikin')
+//     ->with('mahasiswa02','Syadzwina Sahara')
+//     ->with('mahasiswa03','Deliana Putri');
+// });
+
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa')->withmahasiswa01('Khairul Almeer');
+// });
+
+// Route::get('/mahasiswa',function(){
+//     return view('kampus.mahasiswa')
+//     ->withmahasiswa01('Doni Sadikin')
+//     ->withmahasiswa02('Syadzwina Sahara')
+//     ->withmahasiswa03('Deliana Putri');
+// });
+
+// Route::get('/mahasiswa',function(){
+//     $mahasiswa01 = "Indra Kenz",
+//     $mahasiswa02 = "Doni Salmanan",
+//     $mahasiswa03 = "Ulfi Rizkia",
+//     $mahasiswa04 = "Deliana Putri";
+//     return view('kampus.mahasiswa')->with(compact("mahasiswa01","mahasiswa02","mahasiswa03","mahasiswa04"));
+// });
+
+// Route::get('/mahasiswa',function(){
+//     $nama = 'John Paijo Simatupang';
+//     $nilai = 75;
+//     return view('mahasiswa',compact('nama','nilai'));
+// });
+
+// Route::get('/mahasiswa',function(){
+//     $nama = '<u>Elon Musk Batubara</u>';
+//     $nilai = 75;
+//     return view('mahasiswa',compact('nama','nilai'));
+// });
+
+// Route::get('/mahasiswa',function(){
+//     $nama = 'Elon Musk Batubara';
+//     $nilai = 75;
+//     return view('mahasiswa',compact('nama','nilai'));
+// });
+
+// Route::get('/mahasiswa',function(){
+//     $nama = 'Vladimir Putin Harahap';
+//     $nilai = [80,64,30,76,95];
+//     return view('mahasiswa',compact('nama','nilai'));
+// });
+
+// Route::get('mahasiswa',function(){
+//     $nama = 'Tya Kirana Putri';
+//     $nilai = [80,64,30,76,95];
+//     return view('mahasiswa',compact('nama','nilai'));
+// });
+
+?>
